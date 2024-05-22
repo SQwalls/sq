240513967020aa1303075c4f82eb2264dcd4cff0
SQwalls <deepcorna@gmail.com>
2024-05-22 22:22:13 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,92 @@
-const cipherTable = {
-    'あ': '@', 'い': '#', 'う': '/', 'え': '&', 'お': '_',
-    'か': 'D', 'き': 'o', 'く': 'K', 'け': 't', 'こ': 'g',
-    'さ': 'A', 'し': 'j', 'す': 'T', 'せ': 'p', 'そ': 'M',
-    'た': 's', 'ち': 'G', 'つ': 'h', 'て': 'V', 'と': 'z',
-    'な': 'N', 'に': 'n', 'ぬ': 'd', 'ね': 'x', 'の': 'U',
-    'は': 'i', 'ひ': 'L', 'ふ': 'w', 'へ': 'v', 'ほ': 'b',
-    'ま': 'J', 'み': 'u', 'む': 'f', 'め': 'k', 'も': 'C',
-    'や': 'R', 'ゆ': 'q', 'よ': 'r',
-    'ら': 'B', 'り': 'E', 'る': 'S', 'れ': 'e', 'ろ': 'F',
-    'わ': '%', 'を': '+', 'ん': '<',
+const hiragana = [
+    "あいうえお", "かきくけこ", "さしすせそ", "たちつてと", "なにぬねの",
+    "はひふへほ", "まみむめも", "やゆよ", "らりるれろ", "わをん"
+];
+const symbols = [
+    "@#/&_","DoKtg", "AjTpM", "sGhVz", "NndxU",
+    "iLwvb", "JufkC", "Rqr", "BESeF", "%+<"
+];
+const dakutenMap = {
+    'か': 'が', 'き': 'ぎ', 'く': 'ぐ', 'け': 'げ', 'こ': 'ご',
+    'さ': 'ざ', 'し': 'じ', 'す': 'ず', 'せ': 'ぜ', 'そ': 'ぞ',
+    'た': 'だ', 'ち': 'ぢ', 'つ': 'づ', 'て': 'で', 'と': 'ど',
+    'は': 'ば', 'ひ': 'び', 'ふ': 'ぶ', 'へ': 'べ', 'ほ': 'ぼ'
+};
+const handakutenMap = {
+    'は': 'ぱ', 'ひ': 'ぴ', 'ふ': 'ぷ', 'へ': 'ぺ', 'ほ': 'ぽ'
 };
 
-const hiraganaList = Object.keys(cipherTable);
-const symbolList = Object.values(cipherTable);
-
-function shiftCharacterEncrypt(char, shift) {
-    const index = hiraganaList.indexOf(char);
-    if (index === -1) {
-        return char; // If character not found, return as is
-    }
-    const shiftedIndex = (index + shift) % hiraganaList.length;
-    return cipherTable[hiraganaList[shiftedIndex]];
-}
-
-function shiftCharacterDecrypt(char, shift) {
-    const index = symbolList.indexOf(char);
-    if (index === -1) {
-        return char; // If character not found, return as is
-    }
-    const shiftedIndex = (index - shift + hiraganaList.length) % hiraganaList.length;
-    return hiraganaList[shiftedIndex];
-}
-
-function transformText() {
+function performOperation() {
     const inputText = document.getElementById('inputText').value;
     const shiftValue = parseInt(document.getElementById('shiftValue').value, 10);
     const operation = document.getElementById('operation').value;
+    const result = operation === 'encrypt' ? encrypt(inputText, shiftValue) : decrypt(inputText, shiftValue);
+    document.getElementById('result').innerText = result;
+}
 
-    let outputText;
-    if (operation === 'encrypt') {
-        outputText = inputText.split('').map(char => shiftCharacterEncrypt(char, shiftValue)).join('');
-    } else {
-        outputText = inputText.split('').map(char => shiftCharacterDecrypt(char, shiftValue)).join('');
+function encrypt(text, shift) {
+    let encryptedText = '';
+    for (let char of text) {
+        if (dakutenMap[char]) {
+            char = dakutenMap[char];
+            encryptedText += appendMark(encryptCharacter(char, shift), 5);
+        } else if (handakutenMap[char]) {
+            char = handakutenMap[char];
+            encryptedText += appendMark(encryptCharacter(char, shift), 8);
+        } else {
+            encryptedText += encryptCharacter(char, shift);
+        }
     }
-    
-    document.getElementById('outputText').value = outputText;
+    return `${encryptedText}.${shift}`;
+}
+
+function encryptCharacter(char, shift) {
+    for (let i = 0; i < hiragana.length; i++) {
+        let index = hiragana[i].indexOf(char);
+        if (index !== -1) {
+            let shiftedIndex = (index + shift) % symbols[i].length;
+            return symbols[i][shiftedIndex];
+        }
+    }
+    return char;
+}
+
+function decrypt(text, shift) {
+    let [encryptedText, shiftValue] = text.split('.');
+    shiftValue = parseInt(shiftValue, 10);
+    let decryptedText = '';
+    let skip = 0;
+    for (let i = 0; i < encryptedText.length; i++) {
+        if (skip > 0) {
+            skip--;
+            continue;
+        }
+        let char = encryptedText[i];
+        if (char === '<') {
+            decryptedText += removeMark(decryptCharacter(encryptedText.substr(i - 1, 1), shiftValue));
+            skip = 1;
+        } else {
+            decryptedText += decryptCharacter(char, shiftValue);
+        }
+    }
+    return decryptedText;
+}
+
+function decryptCharacter(char, shift) {
+    for (let i = 0; i < symbols.length; i++) {
+        let index = symbols[i].indexOf(char);
+        if (index !== -1) {
+            let shiftedIndex = (index - shift + symbols[i].length) % symbols[i].length;
+            return hiragana[i][shiftedIndex];
+        }
+    }
+    return char;
+}
+
+function appendMark(char, mark) {
+    return `${char}<${mark}`;
+}
+
+function removeMark(char) {
+    return char.replace(/<\d+/, '');
 }
